d85dd846b5c3e198775827c87a334f707a2f22a5
Mathluiz23 <matheus_luizs@hotmail.com>
2025-01-08 12:55:17 -0300

    :sparkles: feat: new function to get user by id to verify if id contains in the group

---
+++ getUserById.js
@@ -0,0 +1,30 @@
+if(item.Type == 'Item') {
+    
+    let group = getGroupByID(19993);
+    //log('id do grupo' + group)
+    
+    let userId = User.ID;
+    //log('user id' + userId)
+    
+    for (let i = 0; i < group.Users.length; i++) {
+        let userInfoGroup = group.Users[i].ID;
+        //log('ID do usuário no grupo: ' + userInfoGroup);
+        
+        if (userId == userInfoGroup) {
+            userIsInGroup = true;
+            break;
+        } else {
+            userIsInGroup = false;
+        }
+    }
+    
+    if (userIsInGroup) {
+        //log('O usuário pertence ao grupo.');
+        item.Fields.Budget_Creator = 'Order Handling';
+        log(item.Fields.Budget_Creator);
+    } else {
+        //log('O usuário NÃO pertence ao grupo.');
+        item.Fields.Budget_Creator = 'EGP';
+        log(item.Fields.Budget_Creator);
+    }
+}
