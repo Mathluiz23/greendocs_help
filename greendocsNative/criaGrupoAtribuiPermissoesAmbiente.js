7713b43a38987a38ee5fa282a9efa28183156ea8
Mathluiz23 <matheus_luizs@hotmail.com>
2025-01-16 13:31:16 -0300

    :sparkles: feat: new function to create group and give enviroment permission

--- greendocsNative/criaGrupoAtribuiPermissoesAmbiente.js
+++ greendocsNative/criaGrupoAtribuiPermissoesAmbiente.js
@@ -1,3 +1,5 @@
+// Usando uma lista fixa de ambientes:
+
 var username = "service@greendocs.net"; //Usuário de serviço
 var password = "senha";
 var encoded = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(username + ":" + password));
@@ -12,3 +14,22 @@ objetoCriar.Acessos = ["Ambiente 1", "Ambiente 2"]; //array com os nomes dos amb
 objetoCriar.Listas = ["Ambiente 1"]; //array com os nomes dos ambientes onde o grupo aparecer como opção nos campos do tipo lista
 
 callWebService(enderecoCriar, "Basic " + encoded, "POST", "application/json", JSON.stringify(objetoCriar), []);
+
+
+// Usando campos do tipo multi-valor com valores correspondentes aos nomes dos ambientes:
+
+
+    var username = "service@greendocs.net"; //Usuário de serviço
+    var password = "senha";
+    var encoded = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(username + ":" + password));
+  
+    //CRIA O GRUPO
+    var enderecoCriar = "https://br00.greendocs.net/api/v2/groups";
+    var objetoCriar = {};
+    objetoCriar.IdConta = 2;
+    objetoCriar.Nome = item.Fields.Short_Name; //Campo do formulário com o nome do grupo
+    objetoCriar.Ativo = true;
+    objetoCriar.Acessos = item.Fields.Ambientes_Acesso;
+    objetoCriar.Listas = item.Fields.Ambientes_Acesso;
+    
+    callWebService(enderecoCriar, "Basic " + encoded, "POST", "application/json", JSON.stringify(objetoCriar), []);
