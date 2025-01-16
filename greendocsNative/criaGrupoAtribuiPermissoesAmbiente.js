954062adedf6aab0c3f2ff13ea6c0a3ee05665f9
Mathluiz23 <matheus_luizs@hotmail.com>
2025-01-16 13:30:26 -0300

    :sparkles: feat: new function to create group and give enviroment permission

---
+++ greendocsNative/criaGrupoAtribuiPermissoesAmbiente.js
@@ -0,0 +1,14 @@
+var username = "service@greendocs.net"; //Usuário de serviço
+var password = "senha";
+var encoded = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(username + ":" + password));
+
+//CRIA O GRUPO
+var enderecoCriar = "https://br00.greendocs.net/api/v2/groups";
+var objetoCriar = {};
+objetoCriar.IdConta = 2;
+objetoCriar.Nome = item.Fields.Short_Name; //Campo do formulário com o nome do grupo
+objetoCriar.Ativo = true;
+objetoCriar.Acessos = ["Ambiente 1", "Ambiente 2"]; //array com os nomes dos ambientes onde o grupo vai ter acesso
+objetoCriar.Listas = ["Ambiente 1"]; //array com os nomes dos ambientes onde o grupo aparecer como opção nos campos do tipo lista
+
+callWebService(enderecoCriar, "Basic " + encoded, "POST", "application/json", JSON.stringify(objetoCriar), []);
