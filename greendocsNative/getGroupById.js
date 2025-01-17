5d36f2f6b83f1eb4ff4c5c0342d5cb7e04f0a71c
Mathluiz23 <matheus_luizs@hotmail.com>
2025-01-17 07:47:58 -0300

    :recycle: refactor: add information at get group by id, ex using for

--- greendocsNative/getGroupById.js
+++ greendocsNative/getGroupById.js
@@ -1,2 +1,8 @@
 let group = getGroupByID(19993);
     //log('id do grupo' + group)
+
+var grupo = getGroupByID(id_grupo);
+log(grupo.Name);
+    for (var i = 0; i < grupo.Users.length; i++) {
+        log(grupo.Users[i].Name + ', ' + grupo.Users[i].Login + ", " + grupo.Users[i].ID);
+    }
