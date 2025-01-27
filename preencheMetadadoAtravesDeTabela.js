9a0e65fac612eb0c3ae45bebeec84d1fb40084db
Mathluiz23 <matheus_luizs@hotmail.com>
2025-01-26 21:34:05 -0300

    :recycle: refactor: put some notes at the code

--- preencheMetadadoAtravesDeTabela.js
+++ preencheMetadadoAtravesDeTabela.js
@@ -1,44 +1,54 @@
-// Código refatorado atualizado 
-
 function Item_AfterEditFields(item) {
-
+    // Verifica se o tipo do item é "GM"
     if (item.Type == 'GM') {
+        // Obtém as tabelas "Acoes_Prioridade_1" e "Acoes_Prioridade_3"
         let tabelap1 = item.Fields.Acoes_Prioridade_1;
         let tabelap3 = item.Fields.Acoes_Prioridade_3;
-        
-        if(!tabelap1) {
+
+        // Se a tabela "Acoes_Prioridade_1" não existir, define o valor como 0
+        // e marca o campo "Acoes_P1_aplicavel" como "Não"
+        if (!tabelap1) {
             tabelap1 = 0;
             item.Fields.Acoes_P1_aplicavel = 'Não';
         }
-        
-         if(!tabelap3) {
+
+        // Se a tabela "Acoes_Prioridade_3" não existir, define o valor como 0
+        // e marca o campo "Acoes_P3_sao_aplicaveis" como "Não"
+        if (!tabelap3) {
             tabelap3 = 0;
             item.Fields.Acoes_P3_sao_aplicaveis = 'Não';
         }
-        
+
+        // Verifica se a tabela "Acoes_Prioridade_1" possui linhas
         if (tabelap1.Rows.length > 0) {
-            log('if tabela p1')
+            log('if tabela p1'); // Loga que está verificando a tabela P1
             for (let i = 0; i < tabelap1.Rows.length; i++) {
                 let row = tabelap1.Rows[i].Columns;
+
+                // Verifica se ao menos um dos campos da linha contém dados
                 if (row.Area_responsavel_ap1 || row.Descricao_da_acao_ap1 || row.Responsavel_pela_acao_ap1 || row.Prazo_ap1) {
-                    item.Fields.Acoes_P1_aplicavel = 'Sim';
-                    break;
+                    item.Fields.Acoes_P1_aplicavel = 'Sim'; // Marca P1 como aplicável
+                    break; // Sai do loop assim que encontrar uma linha válida
                 }
             }
         }
-        
+
+        // Verifica se a tabela "Acoes_Prioridade_3" possui linhas
         if (tabelap3.Rows.length > 0) {
-            log('if tabela p3')
+            log('if tabela p3'); // Loga que está verificando a tabela P3
             for (let i = 0; i < tabelap3.Rows.length; i++) {
                 let row = tabelap3.Rows[i].Columns;
+
+                // Verifica se ao menos um dos campos da linha contém dados
                 if (row.Area_responsavel_ap3 || row.Descricao_da_acao_ap3 || row.Responsavel_pela_acao_ap3 || row.Prazo_ap3) {
-                    item.Fields.Acoes_P3_sao_aplicaveis = 'Sim';
-                    break;
+                    item.Fields.Acoes_P3_sao_aplicaveis = 'Sim'; // Marca P3 como aplicável
+                    break; // Sai do loop assim que encontrar uma linha válida
                 }
             }
         }
     }
 }
+
 
 // código antes da refatoração
 
