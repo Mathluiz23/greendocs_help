285225338a701a4bb196b22c8715820d8e45ff04
Mathluiz23 <matheus_luizs@hotmail.com>
2025-01-30 13:11:08 -0300

    teste

---
+++ tabela/testeTabela.js
@@ -0,0 +1,12 @@
+
+let array = [1, 2, 3];
+
+const newArray = array.length;
+
+const newArray2 = array.length + 1;
+
+if (newArray === newArray2) {
+    console.log("Os arrays são iguais.");
+} else {    
+    console.log("Os arrays são diferentes.");
+}
