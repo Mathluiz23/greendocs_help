d212e25c5a6556895a6bd30fb006ffdb5c962c5d
Mathluiz23 <matheus_luizs@hotmail.com>
2025-01-29 21:19:56 -0300

    :sparkles: feat: new function to create new table just with approved budget

--- tabela/filtraOrcamentosAprovados.js
+++ tabela/filtraOrcamentosAprovados.js
@@ -25,3 +25,31 @@ var TabelaOriginal = item.Fields.Tabela_de_Fornecedores;
             
 
     }
+
+
+    // função otimizada 
+
+    var TabelaOriginal = item.Fields.Tabela_de_Fornecedores;
+var OrcamentosAprovados = item.Fields.Tabela_de_Fornecedores_Apv;
+
+for (var i = 0; i < TabelaOriginal.Rows.length; i++) {
+    log("Verificando orçamento na linha " + i);
+
+    if (TabelaOriginal.Rows[i].Columns["Conclusao_da_Cotacao"] === "Cotação Aprovada") {
+        log("Cotação aprovada encontrada, adicionando nova linha...");
+
+        // Adiciona nova linha à tabela de orçamentos aprovados
+        addTableRow(item, "Tabela_de_Fornecedores_Apv");
+
+        // Obtém a referência da última linha adicionada
+        var novaLinha = OrcamentosAprovados.Rows[OrcamentosAprovados.Rows.length - 1];
+
+        // Copia os valores das colunas relevantes
+        var colunas = ["Fornecedor", "Data_da_Compra", "Condicao_de_Pagamento", "Condicao_de_Frete", "Conclusao_da_Cotacao"];
+        for (var j = 0; j < colunas.length; j++) {
+            novaLinha.Columns[colunas[j]] = TabelaOriginal.Rows[i].Columns[colunas[j]];
+        }
+
+        log("Linha adicionada com sucesso.");
+    }
+}
