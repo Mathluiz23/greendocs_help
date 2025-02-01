de01f421a71543761dffd5b0bf6f25cbe9e15891
Mathluiz23 <matheus_luizs@hotmail.com>
2025-01-31 23:27:19 -0300

    :sparkles: feat: methods to manipulate numbers

---
+++ numbersHelp.js
@@ -0,0 +1,6 @@
+let num = 12.34567;
+
+console.log(num.toFixed(2)); // "12.35"
+console.log(Number.isInteger(num)); // false
+console.log(parseInt("100px")); // 100
+console.log(parseFloat("12.34abc")); // 12.34
