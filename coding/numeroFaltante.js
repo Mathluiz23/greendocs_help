a83838dce8ae0ae50d5e2bc10c38b62b78c6c063
Mathluiz23 <matheus_luizs@hotmail.com>
2025-02-02 23:06:29 -0300

    :sparkles: feat: logic code

---
+++ coding/numeroFaltante.js
@@ -0,0 +1,14 @@
+function numeroFaltante(lista) {
+    let n = lista.length + 1; // 6
+    let somaEsperada = (n * (n + 1)) / 2; // 21
+    let somaReal = lista.reduce((acc, num) => acc + num, 0); 18
+    return somaEsperada - somaReal;
+}
+
+console.log(numeroFaltante([1, 2, 4, 5, 6])); // Saída: 3
+
+
+
+
+
+
