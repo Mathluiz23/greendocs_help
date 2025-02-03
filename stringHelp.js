9e1242cf5d906a74d3f95aa891d059739f8c8b87
Mathluiz23 <matheus_luizs@hotmail.com>
2025-02-02 22:42:04 -0300

    :truck: refactor: moving to utilities folder

--- stringHelp.js
+++
@@ -1,5 +0,0 @@
-let text = "  Hello World!  ";
-console.log(text.trim()); // "Hello World!"
-console.log(text.toUpperCase()); // "  HELLO WORLD!  "
-console.log(text.substring(2, 5)); // "llo"
-console.log(text.includes("World")); // true
