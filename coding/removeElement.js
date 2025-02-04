dd68bd92b182afb03cf5e956c11fca1a0590f1d0
Mathluiz23 <matheus_luizs@hotmail.com>
2025-02-03 22:23:27 -0300

    :sparkles: feat: remove element

---
+++ coding/removeElement.js
@@ -0,0 +1,10 @@
+// without splice
+
+function removeElement(arr, value) {
+    return arr.filter(item => item !== value); // Filtra os elementos diferentes do valor a ser removido
+}
+
+// Testes
+console.log(removeElement([1, 2, 3, 2, 4], 2)); // [1, 3, 4]
+console.log(removeElement([10, 20, 30, 20], 20)); // [10, 30]
+console.log(removeElement([5, 5, 5], 5)); // []
