a63a592725d9e7cdc86e3b1318fde1c821edd0b6
Mathluiz23 <matheus_luizs@hotmail.com>
2025-02-03 22:17:53 -0300

    :sparkles: feat: most freqquent number

---
+++ coding/mostFrequentNumber.js
@@ -0,0 +1,19 @@
+function mostFrequentNumber(arr) {
+    const frequency = {};
+    let maxCount = 0;
+    let mostFrequent = null;
+
+    for (let num of arr) {
+        frequency[num] = (frequency[num] || 0) + 1;
+        if (frequency[num] > maxCount) {
+            maxCount = frequency[num];
+            mostFrequent = num;
+        }
+    }
+
+    return mostFrequent;
+}
+
+// Testes
+console.log(mostFrequentNumber([1, 3, 2, 3, 4, 3, 2, 2, 2])); // 2
+console.log(mostFrequentNumber([10, 20, 20, 10, 10])); // 10
