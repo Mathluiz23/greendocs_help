d48251361eb2398764794a5f451f96276e6bf4f6
Mathluiz23 <matheus_luizs@hotmail.com>
2025-02-03 22:25:32 -0300

    :sparkles: feat: merge array and remove duplicates

---
+++ coding/mergeArray.js
@@ -0,0 +1,7 @@
+function mergeArrays(arr1, arr2) {
+    return [...new Set([...arr1, ...arr2])]; // Une os arrays e remove duplicatas com Set
+}
+
+// Testes
+console.log(mergeArrays([1, 2, 3], [3, 4, 5])); // [1, 2, 3, 4, 5]
+console.log(mergeArrays([10, 20], [20, 30, 40])); // [10, 20, 30, 40]
