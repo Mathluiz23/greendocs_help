9d6d95af61b26112cea8f24514c6622a8eb9f687
Mathluiz23 <matheus_luizs@hotmail.com>
2025-02-04 22:09:02 -0300

    :sparkles: feat: testing function

--- greendocsNativo/getFieldValue.js
+++ greendocsNativo/getFieldValue.js
@@ -1,1 +1,9 @@
 var idFieldValue = getFieldValueID(item, nomeInterno);
+
+function getFieldValueID(item, nomeInterno) {
+    var fieldValue = item.Fields[nomeInterno];
+    if (fieldValue && fieldValue.Rows) {
+        return fieldValue.Rows[0].Columns.ID;
+    }
+    return null;
+}
