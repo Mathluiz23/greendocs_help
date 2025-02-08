cd0d1f2edd9bd6c0984c38c2164e3535a20912b6
Mathluiz23 <matheus_luizs@hotmail.com>
2025-02-08 08:10:18 -0300

    :book: comments

--- coding/isPrime.js
+++ coding/isPrime.js
@@ -2,6 +2,7 @@ function isPrime(n) {
     if (n < 2) return false; // Números menores que 2 não são primos
 
     for (let i = 2; i <= Math.sqrt(n); i++) {
+        // math.sqrt(n) é a raiz quadrada de n, que é o maior número que precisamos testar
         if (n % i === 0) return false; // Se for divisível por qualquer número além de 1 e ele mesmo, não é primo
     }
 
