49afe77d6803bbab3c531a2efccda4a676ce967f
Mathluiz23 <matheus_luizs@hotmail.com>
2025-02-10 21:56:11 -0300

    :truck: refactor: moving fcuntion to specific folder

--- convertToSlug.js
+++
@@ -1,6 +0,0 @@
-const toSlug = (str) => 
-    str.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
-       .replace(/\s+/g, "-").replace(/[^\w-]/g, "");
-  
-  console.log(toSlug("Título de Exemplo!")); // "titulo-de-exemplo"
-  
