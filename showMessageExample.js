0b0fef91e1a95bdb2eeb832d722a9ea43a735db4
Mathluiz23 <matheus_luizs@hotmail.com>
2025-02-10 21:54:47 -0300

    :truck: refactor: moving fcuntion to specific folder

--- showMessageExample.js
+++
@@ -1,6 +0,0 @@
-if (item.Type == 'GM'){
-    if (action.InternalName=='Avaliacao_Concluida'){
-        showMessage('Você tem certeza de que o anexo incluído é o FO-COR-GER-QSMS-011?');
-        // abort("No abort o processo é finalizado e força o usuário a não seguir o fluxo");
-    }
-}
