48e8991692427ed81d7322d85967d5083fed9359
Mathluiz23 <matheus_luizs@hotmail.com>
2025-02-13 22:13:28 -0300

    :truck: refactor: moving fcuntion to specific folder

--- userBelongsToGroup.js
+++
@@ -1,9 +0,0 @@
-function verificarUsuarioNoGrupo(usuario, grupo) {
-    return grupo.includes(usuario);
-  }
-  const grupo = ['Maria', 'João', 'Carlos'];
-  const usuario = 'João';
-  
-  const resultado = verificarUsuarioNoGrupo(usuario, grupo);
-  console.log(resultado); // true (pois João está no grupo)
-  
